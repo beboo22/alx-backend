ad3459db349fdc74eebf0c0cf68fae48d816f115
beboo22 <mt557829@gmail.com>
2024-01-30 22:40:08 +0200

    task

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
 """ FIFOCache class
 """
+from collections import deque
 BaseCaching = __import__('BaseCaching').BaseCaching
+
+
 class FIFOCache(BaseCaching):
+    """ FIFOCache class
+    """
     def __init__(self):
+        self.order_of_arrival = deque()
         super().__init__()
 
     def put(self, key, item):
         if key not in self.cache_data:
-            if key is None or item is None:
-                pass
-            else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key is None or item is None:
+                    pass
+                else:
+                    old_key = self.order_of_arrival.popleft()
+                    print("DISCARD: {}".format(old_key))
+                    del self.cache_data[old_key]
+            if key is not None or item is not None:
                 self.cache_data[key] = item
+                self.order_of_arrival.append(key)
+            else:
+                pass
 
     def get(self, key):
         if key in self.cache_data:
