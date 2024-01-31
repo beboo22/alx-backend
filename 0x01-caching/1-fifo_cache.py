bcac0b830542f4e2ad78d6fcd79499c866a66cce
beboo22 <mt557829@gmail.com>
2024-01-31 18:40:43 +0200

    task

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,19 +17,27 @@ class FIFOCache(BaseCaching):
         '''assign to the dictionary `self.cache_data` the
            `item` value for the key `key`
         '''
-        if key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                if key is None or item is None:
-                    pass
-                else:
-                    old_key = self.order_of_arrival.popleft()
-                    print("DISCARD: {}".format(old_key))
-                    del self.cache_data[old_key]
-            if key is not None or item is not None:
-                self.cache_data[key] = item
-                self.order_of_arrival.append(key)
-            else:
-                pass
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+        # if key not in self.cache_data:
+        #     if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        #         if key is None or item is None:
+        #             pass
+        #         else:
+        #             old_key = self.order_of_arrival.popleft()
+        #             print("DISCARD: {}".format(old_key))
+        #             del self.cache_data[old_key]
+        #     if key is not None or item is not None:
+        #         self.cache_data[key] = item
+        #         self.order_of_arrival.append(key)
+        #     else:
+        #         pass
 
     def get(self, key):
         '''return the value in `self.cache_data` linked to `key`
