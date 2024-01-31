da1223a26f7730dfd783cfbfef295e8fd08d2d16
beboo22 <mt557829@gmail.com>
2024-01-31 19:36:54 +0200

    task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,8 +20,8 @@ class LIFOCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem()
-                print("DISCARD:", last_key)
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD: {}".formate(last_key))
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,18 +11,19 @@ class LRUCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
+        self.order_of_arrival = deque()
 
     def put(self, key, item):
         """Adds an item in the cache.
         """
         if key is None or item is None:
             return
-        if key not in self.cache_data:
+        if key not in self.cache_data and item not in self.cache_data:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
+                last_key, _ = self.cache_data.popitem()
+                print("DISCARD: {}".format(last_key))
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+        self.cache_data.move_to_end(key, last=False)
 
     def get(self, key):
         '''return the value in `self.cache_data` linked to `key`
