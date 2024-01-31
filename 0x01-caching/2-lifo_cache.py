070ca95e1cea1d7cf86d56ec7453464d4ac0003b
beboo22 <mt557829@gmail.com>
2024-01-31 18:30:19 +0200

    task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -27,47 +27,9 @@ class LIFOCache(BaseCaching):
         self.cache_data.move_to_end(key, last=True)
     
     def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
         if key in self.cache_data:
             return self.cache_data[key]
         else:
             return None
-
-# def put(self, key, item):
-    #     """ Adds an item to the cache using LIFO algorithm """
-    #     if key is not None and item is not None and key not in self.cache_data:
-    #         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-    #             last_key = list(self.cache_data.keys())[-1]
-    #             del self.cache_data[last_key]
-    #             print(f"DISCARD: {last_key}")
-    #     elif key is not None and item is not None and key  in self.cache_data:
-    #         print("DISCARD: {}".format(key))
-    #         del self.cache_data[key]
-    #     self.cache_data[key] = item
-    
-    
-    # def put(self, key, item):
-    #     if key is not None and item is not None:
-    #         if key not in self.cache_data:
-    #             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-    #                 recent_key = self.order_of_arrival.pop()
-    #                 print("DISCARD: {}".format(recent_key))
-    #                 del self.cache_data[recent_key]
-                
-    #         else:
-    #             print("DISCARD: {}".format(key))
-    #             del self.cache_data[key]
-    #         self.order_of_arrival.append(key)
-    #         self.cache_data[key] = item
-    
-        # if key is not None and item is not None:
-        #     if key not in self.cache_data:
-        #         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-        #             recent_key, _ = self.cache_data.popitem()
-        #             print("DISCARD: {}".format(recent_key))
-        #             del self.cache_data[recent_key]
-                
-        #     else:
-        #         print("DISCARD: {}".format(key))
-        #         del self.cache_data[key]
-        #     self.order_of_arrival.append(key)
-        #     self.cache_data[key] = item
