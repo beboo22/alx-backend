af747bb8fe372b19ceb1419ca2b2b903fac1ab77
beboo22 <mt557829@gmail.com>
2024-01-31 18:51:56 +0200

    task

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,13 +5,12 @@ from collections import deque, OrderedDict
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """ FIFOCache class
     """
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
-        self.order_of_arrival = deque()
 
     def put(self, key, item):
         """Adds an item in the cache.
@@ -19,8 +18,8 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem()
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
