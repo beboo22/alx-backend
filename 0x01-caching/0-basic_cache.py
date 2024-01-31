e36ee592d708fd58d5cf27131920e1588544e9f2
beboo22 <mt557829@gmail.com>
2024-01-31 17:59:32 +0200

    task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" FIFOCache class
+"""
+FIFOCache class
 """
 from BaseCaching import BaseCaching
 
@@ -11,12 +12,17 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
         if key in self.cache_data:
             return self.cache_data[key]
         else:
